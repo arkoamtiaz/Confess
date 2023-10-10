5b6220c318722e192b12f27b727667c1411a1574
arkoamtiaz <147322125+arkoamtiaz@users.noreply.github.com>
2023-10-10 12:55:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Arko-Amtiaz-the-lessful/pen/GRPzvdj](https://codepen.io/Arko-Amtiaz-the-lessful/pen/GRPzvdj).
+
---
+++ script.js
@@ -0,0 +1,10 @@
+function Sweet_Heart(){
+  var message= document.getElementById("text1");
+  message.style.display = "block";
+  Dil_Ki_Baat();
+}
+
+function Dil_Ki_Baat(){
+  var dil_Se= document.getElementById("text2");
+  dil_Se.style.display = "block";
+}
